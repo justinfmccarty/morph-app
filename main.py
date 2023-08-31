996445a98b6442b3d4d78f26a412437af59179fe
justinfmccarty <mccarty.justin.f@gmail.com>
2023-08-31 09:10:59 +0200

    Point of return to changing morpher right side

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
-# Thanks: 
+from flask import Flask, render_template, request, session, make_response, send_file, Response
+import zipfile
+from flask_session import Session
+import numpy as np
+import io
+import os
+import pandas as pd
+import time
+import csv
+
+
+
+# Thanks:
 # https://www.freecodecamp.org/news/how-to-build-a-web-application-using-flask-and-deploy-it-to-the-cloud-3551c985e492/
 
 app = Flask(__name__, static_folder="static/")
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = "my_session"# os.urandom(24)
+
+SESSION_TYPE = "filesystem"
+app.config.from_object(__name__)
+Session(app)
 
 @app.route("/")
 def home():
     return render_template("home.html", greetings="Hello, world!")
+
 
 @app.route("/about")
 def about():
@@ -16,6 +34,52 @@ def about():
 @app.route("/morpher")
 def morpher():
     return render_template("morpher.html")
+
+
+@app.route("/exec-get-cmip")
+def get_cmip():
+    # Your Python function code here
+    df_dict = func1()
+    return {"result": "success", "dataframe": df_dict}
+
+
+def func1():
+    print("Starting function")
+    time.sleep(0.25)
+    print("Creating array")
+    time.sleep(0.25)
+    arr = np.random.rand(1000, 7)
+    print("Transfer array to dataframe")
+    time.sleep(0.25)
+    df = pd.DataFrame(arr).to_dict()
+    session["data"] = df
+    return df
+
+
+@app.route("/exec-morph-epw")
+def morph_epw():
+    # Your Python function code here
+    # df = func1()
+    data = session["data"]
+    df = pd.DataFrame(data)[[0,1,2,3]]
+    print(df)
+    session["data"] = df.to_dict()
+    return {"result": "success"}
+
+
+@app.route("/exec-get-results")
+def get_results():
+    csv = '1,2,3\n4,5,6\n'
+    return Response(
+        csv,
+        mimetype="text/csv",
+        headers={"Content-disposition":
+                 "attachment; filename=myplot.csv"})
+
+    
+    
+    
+    
 
 
 @app.route("/analysis")
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -17,15 +17,69 @@ function logSubmit(event) {
 // form.addEventListener("lock", logSubmit);
 
 const configForm = document.getElementById("configuration")
-configForm.addEventListener("submit", function(e) {
+configForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  
+
   const data = new FormData(configForm);
   let temp = document.getElementById("temp-output")
   temp.innerText = "---" + "\n"
-  for (const [name,value] of data) {
+  for (const [name, value] of data) {
     console.log(name, ":", value)
     temp.innerText += name + ":" + value + "\n"
-  
   }
+  console.log(data.innerText)
 })
+
+
+var text = $('.typewriter').text();
+
+var length = text.length;
+var timeOut;
+var character = 0;
+
+
+(function typeWriter() {
+  timeOut = setTimeout(function () {
+    character++;
+    var type = text.substring(0, character);
+    $('.typewriter').text(type);
+    typeWriter();
+
+    if (character == length) {
+      clearTimeout(timeOut);
+    }
+
+  }, 150);
+}());
+
+async function execGetCMIP() {
+  const response = await fetch('/exec-get-cmip');
+  // let data = await response.text();
+  // console.log(data.json())
+  // .then(response => response.json()) 
+  // // .then(data => console.log(data)) 
+  // .then(data => sessionStorage.setItem("key", data))
+  // .catch(error => console.error(error)); 
+  // sessionStorage.setItem("myData", data)
+
+}
+
+function execMorphEPW() {
+  let data = sessionStorage.getItem("myData");
+  console.log(data)
+  fetch('/exec-morph-epw')
+  //   .then(response => response.json()) 
+  //   .then(data => console.log(data)) 
+  //   .catch(error => console.error(error)); 
+}
+
+
+function execGetResults() {
+  fetch('/exec-get-results')
+    // .then(res => {
+    //   res.json(); // **
+    // }).catch(err => console.log(err));
+  // .then(response => response.json()) 
+  // .then(data => console.log(data)) 
+  // .catch(error => console.error(error)); 
+} 
